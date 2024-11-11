db7871d37b581b27c1a542c65fbfe1432c143b41
nasrashvili <lukanasrashvili1@gmail.com>
2024-11-11 14:08:57 -0500

    add homework

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// let text = "hello world";
+// console.log(text);
+// let yers = 2024;
+// console.log(text + yers);
+
+// პირველი სინტაქსი
+
+const name1 = "luka";
+const surname = "nasrashvili";
+const dayOfWeek = "monday";
+const favoriteActivity = "programmer in";
+
+console.log(name1, surname, dayOfWeek, favoriteActivity);
+let hi = " my name is";
+let todey = "todey is";
+let perfect = "a perfect day for";
+
+console.log(hi + name1, todey + dayOfWeek, perfect + favoriteActivity);
+// მეორე სინტაქსი
+const text1 = hi + name1;
+console.log(text1);
+const text2 = todey + dayOfWeek;
+console.log(text2);
+const text3 = perfect + favoriteActivity;
+console.log(text3);
